b5458e0626dc568ccbc0ac4d26c505e245cfb0ce
Alioune <sallalioune@gmail.com>
2023-05-07 21:56:55 +0000

    correct error on prod setting

--- xassida/prod.settings.py
+++ xassida/prod.settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
