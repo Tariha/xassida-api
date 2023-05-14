d9071a9d4cb2bfc8d616e8158236247f69edd0a9
Alioune <sallalioune@gmail.com>
2023-05-14 03:12:08 +0000

    playbook for database update

--- update_db.sh
+++ update_db.sh
@@ -9,17 +9,20 @@
 processed=()
 
 for xassida; do
-  name=$( echo $xassida | cut -f 4 -d '/'  )
-  author=$( echo $xassida | cut -f 3 -d '/'  )
-  echo "[Updating...] $author --> $name"
-  if [[ " ${processed[@]} " =~ " $name " ]]; then
-    echo "Skipping duplicate xassida $name"
-  else
-    env/bin/python data/parse_xassida.py -x $name
-    env/bin/python data/parse_translations.py -x $name
-    env/bin/python data/parse_author.py -a $author
-    env/bin/python utils/db/insert.py -x $name
-    echo "Processed xassida $name"
-    processed+=("$name")
+  if [[ $xassida == xassidas/**/*.txt ]]; then
+    name=$( echo $xassida | cut -f 4 -d '/'  )
+    author=$( echo $xassida | cut -f 3 -d '/'  )
+    echo "[Updating...] $author --> $name"
+    if [[ " ${processed[@]} " =~ " $name " ]]; then
+      echo "Skipping duplicate xassida $name"
+    else
+      env/bin/python data/parse_xassida.py -x $name
+      env/bin/python data/parse_translations.py -x $name
+      env/bin/python data/parse_author.py -a $author
+      env/bin/python utils/db/insert.py -x $name
+      echo "Processed xassida $name"
+      processed+=("$name")
+    fi
   fi
 done;
+
