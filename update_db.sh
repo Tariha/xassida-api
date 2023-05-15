65180b664cdebc37d5d4a1f845d7ed15fb29f970
Alioune <sallalioune@gmail.com>
2023-05-15 22:06:49 +0000

    update script changed

--- update_db.sh
+++ update_db.sh
@@ -16,10 +16,12 @@ for xassida; do
     if [[ " ${processed[@]} " =~ " $name " ]]; then
       echo "Skipping duplicate xassida $name"
     else
-      env/bin/python data/parse_xassida.py -x $name
-      env/bin/python data/parse_translations.py -x $name
-      env/bin/python data/parse_author.py -a $author
-      env/bin/python utils/db/insert.py -x $name
+      cd /home/root/xassida/data
+      ../venv/bin/python data/parse_xassida.py -x $name
+      ../venv/bin/python data/parse_translations.py -x $name
+      ../venv/bin/python data/parse_author.py -a $author
+      cd /home/root/xassida/utils/db
+      ../../venv/bin/python utils/db/insert.py -x $name
       echo "Processed xassida $name"
       processed+=("$name")
     fi
