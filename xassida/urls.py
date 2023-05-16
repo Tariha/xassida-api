cee1efec5d5b431d8af981eea9a2a3da075e57e3
Linzo99 <sallalioune@gmail.com>
2023-05-16 12:22:12 +0000

    handle static file on debug

--- xassida/urls.py
+++ xassida/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path("api/", include("api.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
